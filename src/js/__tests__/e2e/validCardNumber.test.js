9d80b16e2be01e897342d1287d922a20ed1567d5
comradexlight <comradexlight@gmail.com>
2022-11-08 17:53:02 +0300

    from work to home

--- src/js/__tests__/e2e/validCardNumber.test.js
+++ src/js/__tests__/e2e/validCardNumber.test.js
@@ -1,5 +1,7 @@
 import puppeteer from 'puppeteer';
 
+jest.setTimeout(8000);
+
 describe('Input valid card number', () => {
   let browser;
   let page;
@@ -14,11 +16,33 @@ describe('Input valid card number', () => {
     page = await browser.newPage();
   });
 
-  test('test', async () => {
+  test('Div .valid-widget-check should ass .checked class if card number is valid by pressing the button  ', async () => {
     await page.goto('http:localhost:9000');
+
+    const form = await page.$('.vailid-widget-form');
+    const input = await form.$('.valid-number');
+    const btn = await form.$('.valid-btn');
+
+    await input.type('348302403735658');
+    await btn.click();
+
+    await page.waitForSelector('.checked');
   });
 
-  afterAll(async () => {
+  test('Div .valid-widget-check should ass .checked class if card number is valid by pressing the button  ', async () => {
+    await page.goto('http:localhost:9000');
+
+    const form = await page.$('.vailid-widget-form');
+    const input = await form.$('.valid-number');
+    const btn = await form.$('.valid-btn');
+
+    await input.type('348302403735657');
+    await btn.click();
+
+    await page.waitForSelector('.unchecked');
+  });
+
+  afterEach(async () => {
     await browser.close();
-  })
+  });
 });
